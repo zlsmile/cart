2d8d01cf9ea7b4b891213934cee257b0a6bc00b7
zlsmile <464474686@qq.com>
2020-10-29 16:09:36 +0800

    第一次提交

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use vuex
+const store =new vuex.store({
+  state:{
+    isshow:true
+
+  },
+  getter:{
+
+  },
+  mutations:{
+
+  },
+  action:{
+
+  }
+})
+export default store;
